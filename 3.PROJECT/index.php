7125783e7c6a1d1465151f5f330ebaadcf0ca1c8
Tran Trong Hoa <68962797+TranTrongHoa@users.noreply.github.com>
2020-09-27 22:30:04 +0700

    up

---
+++ 3.PROJECT/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kênh tin tức giải trí - Xã hội - Kenh14.vn</title>
+</head>
+<body>
+    <header>
+    <ul class="nav">
+    <li><a href="#">QUIZZ</a></li>
+    <li><a href="#">MAGAZINE</a></li>
+    <li><a href="#">ĐỌC CHẬM</a></li>
+    <li><a href="#">HIP-HOP NEVA DIE</a></li>
+    </ul>
+    </header>
+</body>
+</html>
