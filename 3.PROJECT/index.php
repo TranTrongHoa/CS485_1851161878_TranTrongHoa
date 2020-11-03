ab3dfbc292f0f9bfb0e4f3d6d9dff939be349981
Tran Trong Hoa <68962797+TranTrongHoa@users.noreply.github.com>
2020-11-04 00:20:50 +0700

    up bài 00:20 04/11/2020

--- 3.PROJECT/index.php
+++ 3.PROJECT/index.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
     <!--Fontawesome -->
     <script src="https://kit.fontawesome.com/1534cbf1d9.js" crossorigin="anonymous"></script>
-
     <!-- Style CSS-->
     <link rel="stylesheet" href="./CSS/style.css">
 </head>
@@ -36,7 +35,6 @@
                         <li><a href="#" title="Hoa hậu Việt Nam 2020">#Hoa hậu Việt Nam 2020</a></li>
                         <li><a href="#" title="Lũ lụt ở miền Trung">#Lũ lụt ở miền Trung</a></li>
                         <li><a href="#" title="Mùa thu Hà Nội">#Mùa thu Hà Nội</a></li>
-
                     </ul>
                 </div>
                 <div class="nav-menu">
@@ -55,7 +53,6 @@
                         <li class="nav-item"><a class="nav-link" href="#">Sport</a></li>
                         <li class="nav-item"><a class="nav-link" href="#">Học đường</a></li>
                         <li class="nav-item"><a class="nav-link" href="#">Video</a></li>
-
                 </div>
                 <!-- Nút bars khi reponsive -->
                 <label for="check" class="navbar-button">
@@ -68,7 +65,6 @@
                     </svg>
                 </label>
                 <input type="checkbox" hidden id="check">
-
                 <!--Menu sau khi responsive sẽ thấy ở bars -->
                 <div class="nav-menu-responsive">
                     <ul>
@@ -101,9 +97,7 @@
                             </path>
                         </svg>
                     </label>
-
                 </div>
-
             </div>
             <!--Banner quảng cáo đầu-->
             <div class="col-md-12 padding-0">
@@ -120,21 +114,22 @@
                         <img src="./image/baiviet_01.jpg" alt="Phép màu trong vụ sạt lở đất ở Trà Leng: Đã cứu sống được 33 người, tiếp tục tìm kiếm 13 nạn nhân mất tích">
                     </a>
                     <h2 class="tieude_baiviet_01">
-                        <a href="#" target="_blank" title="Phép màu trong vụ sạt lở đất ở Trà Leng: Đã cứu sống được 33 người, tiếp tục tìm kiếm 13 nạn nhân mất tích">Phép màu trong vụ sạt lở đất ở Trà Leng: Đã cứu sống được 33 người, tiếp tục tìm kiếm 13 nạn nhân mất tích</a>
+                        <a href="#" target="_blank" title="Phép màu trong vụ sạt lở đất ở Trà Leng: Đã cứu sống được 33 người, tiếp tục tìm kiếm 13 nạn nhân mất tích">Phép
+                            màu trong vụ sạt lở đất ở Trà Leng: Đã cứu sống được 33 người, tiếp tục tìm kiếm 13 nạn nhân
+                            mất tích</a>
                     </h2>
-                    <p class="noidungngan_baiviet_01">
-                        Một quả đồi lớn bị sạt lở đã vùi lấp một ngôi làng tại huyện miền núi tỉnh Quảng Nam, khiến nhiều người mất tích. Thủ tướng Chính phủ yêu cầu các cơ quan liên quan bằng mọi biện pháp cần thiết khẩn trương cứu hộ, cứu ...
-                    </p>
+                    <p class="noidungngan_baiviet_01">Một quả đồi lớn bị sạt lở đã vùi lấp một ngôi làng tại huyện miền núi tỉnh Quảng Nam, khiến nhiều người mất tích. Thủ tướng Chính phủ yêu cầu các cơ quan liên quan bằng mọi biện pháp cần thiết khẩn trương cứu hộ, cứu ...</p>
                 </div>
                 <div class="baiviet_02">
                     <a href="#" target="_blank">
                         <img src="./image/baiviet_02.png" alt="Infographic: Chỉ trong 2 ngày, 5 vụ sạt lở xảy ra liên tiếp khiến gần 20 người chết, nhiều người mất tích và bị thương tại Quảng Nam">
                     </a>
                     <h2 class="tieude_baiviet_02">
-                        <a href="#" target="_blank" title="Infographic: Chỉ trong 2 ngày, 5 vụ sạt lở xảy ra liên tiếp khiến gần 20 người chết, nhiều người mất tích và bị thương tại Quảng Nam">Infographic: Chỉ trong 2 ngày, 5 vụ sạt lở xảy ra liên tiếp khiến gần 20 người chết, nhiều người mất tích và bị thương tại Quảng Nam</a>
+                        <a href="#" target="_blank" title="Infographic: Chỉ trong 2 ngày, 5 vụ sạt lở xảy ra liên tiếp khiến gần 20 người chết, nhiều người mất tích và bị thương tại Quảng Nam">Infographic:
+                            Chỉ trong 2 ngày, 5 vụ sạt lở xảy ra liên tiếp khiến gần 20 người chết, nhiều người mất tích
+                            và bị thương tại Quảng Nam</a>
                     </h2>
                     <p class="noidungngan_baiviet_02">
-
                     </p>
                 </div>
                 <div class="baiviet_03">
@@ -147,7 +142,8 @@
                                 <img src="./image/slideshow_01.jpg" alt="" class="slider-image">
                                 <div class="post-info">
                                     <p>
-                                        <a href="single.php">Show Hương Giang đối đầu cùng antifan: Chịu trách nhiệm nếu bị phát hiện dàn dựng nhưng xem xong cảm giác ngược lại!</a>
+                                        <a href="single.php">Show Hương Giang đối đầu cùng antifan: Chịu trách nhiệm nếu
+                                            bị phát hiện dàn dựng nhưng xem xong cảm giác ngược lại!</a>
                                     </p>
                                 </div>
                             </a>
@@ -157,7 +153,8 @@
                                 <img src="./image/slideshow_02.jpg" alt="" class="slider-image">
                                 <div class="post-info">
                                     <p>
-                                        <a href="single.php">Thuỷ Tiên thông báo ngừng hỗ trợ tại Hải Lăng, Quảng Trị do có nhiều người khá giả "đeo vàng, sơn móng chân" đến nhận tiền</a>
+                                        <a href="single.php">Thuỷ Tiên thông báo ngừng hỗ trợ tại Hải Lăng, Quảng Trị do
+                                            có nhiều người khá giả "đeo vàng, sơn móng chân" đến nhận tiền</a>
                                     </p>
                                 </div>
                             </a>
@@ -177,7 +174,8 @@
                                 <img src="./image/slideshow_04.jpg" alt="" class="slider-image">
                                 <div class="post-info">
                                     <p>
-                                        <a href="single.php">Thủy Tiên lên tiếng về vụ cán bộ thôn thu lại 400 triệu đồng cứu trợ, làm rõ chuyện bà con chê bánh chưng gây xôn xao</a>
+                                        <a href="single.php">Thủy Tiên lên tiếng về vụ cán bộ thôn thu lại 400 triệu
+                                            đồng cứu trợ, làm rõ chuyện bà con chê bánh chưng gây xôn xao</a>
                                     </p>
                                 </div>
                             </a>
@@ -186,14 +184,44 @@
                             <a href="single.php">
                                 <img src="./image/slideshow_05.jpg" alt="" class="slider-image">
                                 <div class="post-info">
-                                    <p>
-                                        <a href="single.php">Dàn sao thế giới "chặt chém" Halloween: Nancy (MOMOLAND) hot nhất Kpop, Kylie chơi trội vẫn để thua bà hoàng Heidi Klum</a>
-                                    </p>
+                                    <p><a href="single.php">Dàn sao thế giới "chặt chém" Halloween: Nancy (MOMOLAND) hot
+                                            nhất Kpop, Kylie chơi trội vẫn để thua bà hoàng Heidi Klum</a></p>
                                 </div>
                             </a>
                         </div>
                     </div>
                 </div>
+
+                <div class="list_baiviet">
+                    <div class="post">
+                        <img src="./image/list_baiviet01.jpg" alt="" class="post-image">
+                        <div class="post-preview">
+                            <h3><a href="#">Cách netizen nhìn nhận về drama antifan sau hơn 1 tuần: “Hương Giang cũng không ngờ, cô tự mắc vào cái bẫy tinh xảo của chính mình”</a></h3>
+                            <a href="#">Star</a> - 1 ngày trước
+                            <p class="preview-text">Netizen đã đưa ra rất nhiều quan điểm từ câu chuyện ồn ào của Hương Giang và antifan, nhưng đa số đều khẳng định nước ...</p>
+                            <a href="#" class="btn read-more">Đọc thêm</a>
+                        </div>
+                    </div>
+                    <div class="post">
+                        <img src="./image/list_baiviet02.jpg" alt="" class="post-image">
+                        <div class="post-preview">
+                            <h3><a href="#">Như Chưa Hề Có Cuộc Chia Ly chính thức trở lại: 45 năm lưu lạc, 8 tiếng tìm thấy nhau nhờ cộng đồng mạng, 5 anh em khóc ngất trên sân khấu</a></h3>
+                            <a href="#">TV Show</a> - 1 ngày trước
+                            <p class="preview-text">Cuộc đoàn tụ của 5 anh em trong tập trở lại của Như Chưa Hề Có Cuộc Chia Ly khiến khán giả cũng không kìm được nước mắt.</p>
+                            <a href="#" class="btn read-more">Đọc thêm</a>
+                        </div>
+                    </div>
+                    <div class="post">
+                        <img src="./image/list_baiviet03.jpg" alt="" class="post-image">
+                        <div class="post-preview">
+                            <h3><a href="#">Chung Hán Lương - Lý Tiểu Nhiễm tái hợp ở phim chuyển thể của Phỉ Ngã Tư Tồn, ngược nhau tơi tả như 10 năm trước?</a></h3>
+                            <a href="#">Ciné</a> - 8 giờ trước
+                            <p class="preview-text">Sau lần hợp tác từ 10 năm trước, cuối cùng Lý Tiểu Nhiễm và Chung Hán Lương có thể sẽ được tái hợp trên màn ảnh nhỏ.</p>
+                            <a href="#" class="btn read-more">Đọc thêm</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <!-- Quảng cáo bên cạnh-->
             <div class="col-md-4 padding-0">
@@ -207,17 +235,14 @@
                     <a href="#" target="_blank">
                         <img src="./image/poster_quangcao_02.jpg"></img>
                     </a>
-
                 </div>
             </div>
         </div>
     </div>
     <!-- JQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
     <!--Slick Carousel -->
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-
     <!-- My JS-->
     <script src="./JS/javascript.js"></script>
 </body>
